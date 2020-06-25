a193ee73311b9d61cab0ce1037a52825a8d231fd
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-06-25 11:20:03 -0700

    Add files via upload

---
+++ Getting Started/Sum.c
@@ -0,0 +1,16 @@
+// WAP to print sum of 2 numbers
+// Author Aditya Bobde
+#include<stdio.h>
+main()
+{
+
+    int a,b,c;
+    printf("\n The first number is");
+    scanf("%d",&a);
+
+    printf("The second number is");
+    scanf("%d",&b);
+
+    c = a+b;
+    printf("The answer is %d\n",c);
+}
