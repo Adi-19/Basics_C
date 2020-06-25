9988cab721556575f44ed10f8eaee373bf4dbe58
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-06-25 11:27:45 -0700

    Add files via upload

---
+++ Getting Started/getchar.c
@@ -0,0 +1,13 @@
+//WAP to show use of getchar() and putchar
+//Author : Aditya Bobde
+#include<stdio.h>
+
+main()
+{
+    char ch;
+    ch = getchar();  // can be used insted of scanf()
+    putchar(ch);     // can be used insted of printf()
+}
+
+
+
