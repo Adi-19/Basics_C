f223fbed929ab78b6bc8c8d7ce7486e43c50448e
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-06-26 10:46:37 -0700

    Add files via upload

---
+++ Decision Making/If.c
@@ -0,0 +1,15 @@
+// WAP to show if person is eligible for Voting using if statement
+// Author : Aditya Bobde
+
+void main()
+{
+    int age;
+    printf("Enter your age");
+    scanf("%d",&age);
+
+    if(age>18)
+    {
+
+        printf("\n You are eligable for Voting");
+    }
+}
