eab1e9c3c54e6a166136f0abcb3d70a1810589b6
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-06-27 11:01:11 -0700

    Add files via upload

---
+++ Control Loops/For Loop.c
@@ -0,0 +1,15 @@
+// WAP to show the use of For loop (Print 0 to 10 numbers)
+//Author : Aditya Bobde
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i, n=10;
+    for(i=0;i<=n;i++)
+    {
+        printf("\n%d",i);
+    }
+
+}
